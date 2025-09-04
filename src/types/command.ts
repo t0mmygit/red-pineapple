78e1be4b5f6b2e6b041c1f9e5f1a3438820db791
t0mmygit <tommychinliwei@gmail.com>
2025-09-04 14:04:26 +0800

    feat(types): Add EventData definition for database operation and autocomplete property to Command.

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,8 +1,13 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  AutocompleteInteraction,
+  ChatInputCommandInteraction,
+  SlashCommandBuilder
+} from 'discord.js';
 import { Middleware } from './middleware.js';
 
 export interface Command {
   data: SlashCommandBuilder,
   middlewares: Middleware[],
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>,
+  autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>
 }
