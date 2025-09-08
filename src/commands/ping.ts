b169db5d5fc857765dec297869fbe9691a1c6727
t0mmygit <tommychinliwei@gmail.com>
2025-09-09 00:38:27 +0800

    refactor(ping): Remove middleware

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,10 @@
 import { ChatInputCommandInteraction } from 'discord.js';
 import createSlashCommand from '../utils/createSlashCommand.js';
-import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 export default createSlashCommand({
   name: 'ping',
   description: 'Replies with pong!',
-  middlewares: [isAuthenticated],
+  middlewares: [],
   execute: async (interaction: ChatInputCommandInteraction) => {
     await interaction.reply('Pinging...');
 
